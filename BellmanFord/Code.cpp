f90639a04451a7431bec1c017bb2ebc150ff7d19
DEEPESH SAXENA <deepeshsaxena18@gmail.com>
2026-03-12 12:55:07 +0530

    12 M A R C H

--- BellmanFord/Code.cpp
+++ BellmanFord/Code.cpp
@@ -16,15 +16,17 @@ int main() {
     int src;
     cin >> src;
 
-    vector<int> dist(n, 1e9);
+    vector<int> dist(n, INT_MAX);
     dist[src] = 0;
 
-    for(int i = 0; i < n - 1; i++) {
-        for(auto e : edges) {
+    for(int i = 1; i <= n-1; i++) {
+        for(auto edge : edges) {
             int u, v, w;
-            tie(u, v, w) = e;
-            if(dist[u] != 1e9 && dist[u] + w < dist[v])
+            tie(u, v, w) = edge;
+
+            if(dist[u] != INT_MAX && dist[u] + w < dist[v]) {
                 dist[v] = dist[u] + w;
+            }
         }
     }
 
